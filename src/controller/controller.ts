6459f91386296ead272fabc3ee0af2baae7ae42f
Fred <fredjgermain@gmail.com>
2020-12-09 14:38:23 -0500

    revert

--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -1,13 +1,17 @@
 import express, {Request, Response} from 'express'; 
 
 function Access(req:Request, res:Response) { 
+<<<<<<< HEAD
   return res.status(200).send("<h1>Api is ready</1>"); 
+=======
+  return res.status(200).send("<1>Api is ready</h1>"); 
+>>>>>>> parent of 2ff06da... Installs
 } 
 
 function MakeController() { 
   const controller = express.Router(); 
   controller.get('/api/', Access); 
-  return controller; 
+  return controller;
 } 
 const controller = MakeController(); 
 
