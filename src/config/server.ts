5287348d254b75a9c960060499b5940c1e938aea
Fred <fredjgermain@gmail.com>
2020-12-09 14:52:10 -0500

    import cors

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,9 +1,31 @@
 import express from 'express'; 
 import {controller} from '../controller/controller'; 
 
-const app = express()
+import cors from "cors"; 
 
+const app = express();
+app.use(cors()); 
 app.use(controller); 
+
+const options: cors.CorsOptions = {
+/*allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'X-Access-Token',
+],
+credentials: true,
+methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',*/
+//origin: "http://localhost:3000", 
+origin: "https://react-mongoose-demo.herokuapp.com", 
+//origin: API_URL, 
+//preflightContinue: false,
+};
+
+app.use(cors(options)); 
+app.options('*', cors(options));
+
 const PORT : string|number = process.env.PORT || 5000; 
 
 /*app.use("*",(req, res) =>{
