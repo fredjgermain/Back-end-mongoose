d50df3d57bba13120489eb8a34196000dc7325c4
Fred <fredjgermain@gmail.com>
2021-04-02 22:07:05 -0400

    mod answers

--- src/mockdb/mockdata/answers.mockdata.ts
+++ src/mockdb/mockdata/answers.mockdata.ts
@@ -1,13 +1,14 @@
 import mongoose from 'mongoose'; 
-
+import { patients } from './patients.mockdata'; 
+import { questions } from './questions.mockdata'; 
 
 // FORM -----------------------------------------
 export const answers = [
   { 
     _id: new mongoose.Types.ObjectId(), 
-    patient: '', 
+    patient: patients[0], 
     date: new Date(), 
-    question: '', 
+    question: questions[0], 
     answer: -1, 
   }
 ] 
